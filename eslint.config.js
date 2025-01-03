d89b84330001cc58701edb3368c3ce427a4d1292
zhm <frontzhm@163.com>
2025-01-03 18:00:08 +0800

    feat: vite的常规配置

--- eslint.config.js
+++ eslint.config.js
@@ -14,11 +14,28 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      // @ts-ignore
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    // @ts-ignore
     rules: {
       ...reactHooks.configs.recommended.rules,
+      /**
+       * off表示关闭规则，也可以使用数字0代替
+       * warn表示警告，也可以使用数字1代替
+       * error表示错误，也可以使用数字2代替
+       */
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      '@typescript-eslint/no-unused-vars': 'off',
+      // 不使用var
+      'no-var': 'error',
+      // 没有console
+      'no-console': 'warn',
+      // 没有debugger
+      'no-debugger': 'warn',
+      // 没有alert
+      'no-alert': 'warn',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // @配置根目录, 默认是当前目录,index.html所在目录
+  base: '/', // @配置基础路径, 默认是/,如果配置/base/，那么访问路径就是http://localhost:3000/base/index.html
+  publicDir: 'public', // @配置静态资源目录, 默认是public
   // @配置别名
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  resolve: { alias: { '@': '/src', }, },
+  server: {
+    // port: 3000, // @配置端口号, 默认是3000
+    // open: true, // @配置是否自动打开浏览器, 默认是false
+    proxy: { // @配置代理
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
   },
+
 })
