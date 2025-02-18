dcaf723ed63632cfbe4e26a3b823f6632d566ad4
zhm <frontzhm@163.com>
2025-02-18 14:55:02 +0800

    feat: 加cssModule

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
-import request from '@/utils/request'
+import React from 'react'
+import style from './index.module.less'
+import { Button } from 'antd'
 
 type HomeProps = {}
 const Home: React.FC<HomeProps> = () => {
-  useEffect(() => {
-    request.get('/api/home').then((res: any) => {
-      console.log(res)
-    })
-  }, [])
-  return <div>Home</div>
+  return (
+    <div className={style['page-home']}>
+      <h1 className={style.title}>Home</h1>
+      <p className={style.content}>This is the home page.</p>
+      <Button>按钮</Button>
+    </div>
+  )
 }
 
 Home.displayName = 'Home'
